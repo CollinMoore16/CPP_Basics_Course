572bbf58f040d6827f9b92a5436457ef4b74bdfb
CollinMoore16 <93558570+CollinMoore16@users.noreply.github.com>
2022-11-05 16:20:14 -0400

    Add files via upload

---
+++ 1_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+int integer1;
+std :: cout << "input your first number " << std :: endl;
+std :: cin >> integer1;
+
+int integer2;
+std :: cout << "input your second number " << std :: endl;
+std :: cin >> integer2;
+
+std :: cout << integer1 + integer2 << std :: endl;
+}
